e5b3651dbda7b6451f61a96e432ddd5ab2f52106
DTennant <Tennant_1999@outlook.com>
2017-07-07 10:26:06 +0800

    update

--- NOIP/NOIP2015D1T1.cpp
+++ NOIP/NOIP2015D1T1.cpp
@@ -4,7 +4,7 @@ int main() {
     scanf("%d",&n);
     int x = 1, y = (n + 1) / 2;
     map[x][y] = 1;
-    for (int i = 2; i <= n * n; i++){
+    for (int i = 2; i <= n * n; i++) {
         if (x == 1 && y != n) x = n, y++;
         else if (x != 1 && y == n) x--, y = 1;
         else if (x == 1 && y == n) x++;
@@ -12,8 +12,8 @@ int main() {
         else x++;
         map[x][y] = i;
     }
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++)
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++)
             printf("%d%c", map[i][j], j == n ? '\n' : ' ');
     }
     return 0;
